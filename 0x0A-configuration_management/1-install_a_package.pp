757bea9ada27620e2453a5aa3ed9fb8f15d13355
edisson11osorio <osorioedisson08@gmail.com>
2022-04-19 16:22:38 -0500

    fix_errors

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,5 @@
 package { 'puppet-lint':
   ensure   => '2.5.0',
   provider => 'gem',
-  source   => 'https://rubygems.org',
+  source   => 'https://rubygems.org/gems/puppet-lint/versions/2.5.0,
 }
