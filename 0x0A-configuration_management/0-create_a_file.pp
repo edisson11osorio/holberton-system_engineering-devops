b37e5f31cd6fb8bd504a8606c7df0ed08c145c82
edisson11osorio <osorioedisson08@gmail.com>
2022-04-19 16:12:38 -0500

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file 
+file { 'school':
+  ensure  => present,
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
