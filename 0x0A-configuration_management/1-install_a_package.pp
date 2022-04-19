5323b7596255efce8dbcbed9ac34f0e0db570087
edisson11osorio <osorioedisson08@gmail.com>
2022-04-19 16:26:46 -0500

    requerimiento fallido corregido

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # install puppet-lint
+
 package { 'puppet-lint':
   ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org/gems/puppet-lint/versions/2.5.0,
+  provider => gem,
+  source   => 'http://rubygems.org',
 }
